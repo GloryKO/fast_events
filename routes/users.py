f107b781d2826c01c568297abdf882c0efcc28c7
GloryKO <koladeg2022@gmail.com>
2022-09-05 10:35:57 -0700

    secured all enpoints and added authentication with OAuth and JWT

--- routes/users.py
+++ routes/users.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter,HTTPException, status
-from models.users import User , UserSignIn
+from models.users import User
 from typing import Optional, List
 from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 from models.events import Event
 from fastapi import APIRouter, HTTPException, status
 from database.connection import Database
-from models.users import User, UserSignIn
+from models.users import User
 from auth.hashpassword import HashPassword
 hash_password = HashPassword()
 user_router = APIRouter(
@@ -17,7 +17,11 @@ user_database = Database(User)
 user_router = APIRouter (
    tags = ["User"]
 )
-
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from auth.jwt_handler import create_access_token
+from models.users import User
+from models.users import User, TokenResponse
 # users ={}
 
 # @user_router.post("/signup")
@@ -52,8 +56,8 @@ user_router = APIRouter (
 #     }
 
 @user_router.post("/signup")
-async def sign_user_up(user:User):
-    user_exist = await User.find_one(User.email==user.email)
+async def sign_user_up(user:OAuth2PasswordRequestForm = Depends()):
+    user_exist = await User.find_one(User.email==user.username)
     if user_exist:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
@@ -61,11 +65,15 @@ async def sign_user_up(user:User):
             )
     hashed_password = hash_password.create_hash(user.password)
     user.password = hashed_password
-    
     await user_database.save(user)
-    return { "message": "User created successfully"}
+    if hash_password.verify_hash(user.password,user_exist.password):
+        access_token = create_access_token(user_exist.email)
+    return { 
+        "access_token": access_token,
+        "token_type": "Bearer"
+    }
 
-@user_router.post("/signin")
+@user_router.post("/signin",response_model=TokenResponse)
 async def sign_user_in(user: UserSignIn) -> dict:
     user_exist = await User.find_one(User.email == 
     user.email)
